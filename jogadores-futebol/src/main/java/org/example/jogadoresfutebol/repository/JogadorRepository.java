37544f75000a0bd236e4ba958575b93b69d858a1
Leandro <53880351+leandro-25@users.noreply.github.com>
2024-11-25 18:51:55 -0300

    Delete jogadores-futebol directory

--- jogadores-futebol/src/main/java/org/example/jogadoresfutebol/repository/JogadorRepository.java
+++
@@ -1,7 +0,0 @@
-package org.example.jogadoresfutebol.repository;
-
-import org.example.jogadoresfutebol.model.Jogador;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface JogadorRepository extends JpaRepository<Jogador, Long> {
-}
