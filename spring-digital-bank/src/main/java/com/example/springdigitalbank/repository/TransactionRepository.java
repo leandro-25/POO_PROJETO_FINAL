82d9d3cd0560ed00493a18090eb12d65ba791950
Leandro <53880351+leandro-25@users.noreply.github.com>
2024-11-25 12:48:11 -0300

    Delete spring-digital-bank directory

--- spring-digital-bank/src/main/java/com/example/springdigitalbank/repository/TransactionRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.springdigitalbank.repository;
-
-import com.example.springdigitalbank.model.Transaction;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface TransactionRepository extends JpaRepository<Transaction, Long> {
-}
