50364aa1900944f352feadfafaa7d23fbb931dc4
Leandro <53880351+leandro-25@users.noreply.github.com>
2024-11-27 13:17:57 -0300

    Add files via upload

---
+++ jogadores-futebol/src/main/java/org/example/jogadores_futebol/JogadoresFutebolApplication.java
@@ -0,0 +1,13 @@
+package org.example.jogadores_futebol;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JogadoresFutebolApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JogadoresFutebolApplication.class, args);
+	}
+
+}
