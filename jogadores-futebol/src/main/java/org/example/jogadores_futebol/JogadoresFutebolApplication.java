b89512a2c20dfc515a224150a01b270d784d7a7c
Thiago Foganholo <142689424+ThiagoFoganholo@users.noreply.github.com>
2024-11-27 17:01:16 -0300

    Update JogadoresFutebolApplication.java

--- jogadores-futebol/src/main/java/org/example/jogadores_futebol/JogadoresFutebolApplication.java
+++ jogadores-futebol/src/main/java/org/example/jogadores_futebol/JogadoresFutebolApplication.java
@@ -3,11 +3,23 @@ package org.example.jogadores_futebol;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+
+/**
+ * Classe principal da aplicação Spring Boot.
+ * Responsável por inicializar e configurar o aplicativo.
+ */
 @SpringBootApplication
 public class JogadoresFutebolApplication {
 
+	/**
+	 * Método principal (entry point) da aplicação.
+	 * Executa o aplicativo Spring Boot.
+	 *
+	 * @param args Argumentos da linha de comando (opcional).
+	 */
 	public static void main(String[] args) {
 		SpringApplication.run(JogadoresFutebolApplication.class, args);
 	}
 
 }
+
