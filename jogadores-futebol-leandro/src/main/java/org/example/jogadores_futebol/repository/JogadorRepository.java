3d938b257434a461fae492a80523873b16bde129
Thiago Foganholo <142689424+ThiagoFoganholo@users.noreply.github.com>
2024-11-28 08:22:25 -0300

    Delete jogadores-futebol-leandro directory

--- jogadores-futebol-leandro/src/main/java/org/example/jogadores_futebol/repository/JogadorRepository.java
+++
@@ -1,7 +0,0 @@
-package org.example.jogadores_futebol.repository;
-
-import org.example.jogadores_futebol.model.Jogador;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface JogadorRepository extends JpaRepository<Jogador, Long> {
-}
